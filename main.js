c5a93579d30252989d45ef1b2d5f9f6546a88bba
Luke Dosen <lddosen@gmail.com>
2018-10-29 15:39:54 -0500

    added lander page

---
+++ main.js
@@ -0,0 +1,22 @@
+var letters = anime({
+    targets: 'h1 .text-wrapper',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+})
+
+var signin = anime({
+    targets: '.get-started',
+    opacity: 100,
+    duration: 1000,
+    easing: "easeInExpo",
+    delay: 500
+})
+
+var signin = anime({
+    targets: '.sign-up',
+    opacity: 100,
+    duration: 1000,
+    easing: "easeInExpo",
+    delay: 800
+})
