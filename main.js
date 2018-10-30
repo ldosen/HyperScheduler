f12683f4b1dc6369816d2d386ff42ff42a9b7ffc
Luke Dosen <lddosen@gmail.com>
2018-10-30 12:28:45 -0500

    mainpage prototype complete

--- main.js
+++ main.js
@@ -13,10 +13,33 @@ var signin = anime({
     delay: 500
 })
 
-var signin = anime({
+var signup = anime({
     targets: '.sign-up',
     opacity: 100,
     duration: 1000,
     easing: "easeInExpo",
     delay: 800
+})
+
+var availability = anime({
+    targets: '.availability',
+    translateY: [0, "1.1em"],
+    translateZ: 0,
+    duration: 750,
+})
+
+var calendar = anime({
+    targets: '.calendar',
+    translateY: [0, "1.1em"],
+    translateZ: 0,
+    duration: 750,
+    delay: 50
+})
+
+var find_subs = anime({
+    targets: '.find-subs',
+    translateY: [0, "1.1em"],
+    translateZ: 0,
+    duration: 750,
+    delay: 70
 })
