be5050d00a6da73e2e1ef7698a8251943a733c50
Austin <aedington@luc.edu>
2018-12-04 13:31:51 -0600

    more work on availability, WIP

--- availability.js
+++ availability.js
@@ -1,28 +1,41 @@
 function submit_availability(){
-    "use strict";
-
     const db = firebase.database();
 
-    let day = document.getElementById('days');
+    var user = firebase.auth().currentUser;
+    if(user != null) {
+        user.providerData.forEach(function (profile) {
+            console.log("name: " + profile.displayName);
+            var name = user.displayName;
+        });
+    }
+    
+
     let submitBtn = document.getElementById('submit');
-    let checkbox = document.getElementById('checkbox');
     let checkmark = document.getElementById('checkmark');
+    var day = document.getElementById('days');
+    var checkbox = document.getElementById('checkbox');
+    var container = document.getElementById('container');
+    var checkboxes = document.getElementsByTagName('input');
 
-    checkbox.addEventListener('change', function() {
+    console.log(day);
 
-        var what_day = day.value;
-
-        if(this.checked){
-
-            var time = this.checked
+    submitBtn.addEventListener('click', function() {
+        if(checkbox.checked){
+        var what_day = day.textContent;
+            var time = container.textContent;
             console.log(what_day + ", " + time + " was clicked!");
             var query = "availability_fall_2018/days/" + what_day + "/" + time;
 
-            db.ref(query)
-
-            .catch((e) =>{
+            var r = db.ref(query);
+            if(r.child(query) != name ){
+            r.child(query).set(name)
+            }
+            r.catch((e) =>{
                 console.log (e);
             });
         }
+        alert("Congrats! Your Availability Has Been Updated!");
     });
 }
+
+submit_availability();
